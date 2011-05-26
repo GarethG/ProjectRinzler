a6362272c31c62f4457d59d2c07d4a78c848599f
James Taylor <jamestaylor151@gmail.com>
2011-05-26 22:46:34 +0100

    Adding various files from testing

---
+++ ros_tutorials/beginner_tutorials/build/CMakeFiles/ROSBUILD_gensrv_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "../src/beginner_tutorials/msg"
+  "../src/beginner_tutorials/srv"
+  "../msg_gen"
+  "../srv_gen"
+  "../msg_gen"
+  "../srv_gen"
+  "CMakeFiles/ROSBUILD_gensrv_lisp"
+  "../srv_gen/lisp/AddTwoInts.lisp"
+  "../srv_gen/lisp/_package.lisp"
+  "../srv_gen/lisp/_package_AddTwoInts.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_gensrv_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
