be503428c90c38e5a9f3c0e67e9334bf9414ada0
GarethG <info@gareth-griffiths.co.uk>
2011-05-31 15:25:49 +0100

    added new svp driver
    initialy using code from the internet at
    
    http://answers.ros.org/question/1081/how-can-ros-communicate-with-my-microcontroller
    
    This is for 2 way comms with a microcontroller so should be perfect. But its really messy
    
    GG

---
+++ Drivers/svp/build/CMakeFiles/svp.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/garethg/dev/ProjectRinzler/Drivers/svp/src/svp.cpp" "/home/garethg/dev/ProjectRinzler/Drivers/svp/build/CMakeFiles/svp.dir/src/svp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
