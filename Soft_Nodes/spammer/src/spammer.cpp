7f8defca9b60d6a45d4d21a884fbc36a25de2335
everto151 <jamestaylor151@gmail.com>
2011-06-14 11:05:35 +0100

    Pitch control has been tested in software and is working

--- Soft_Nodes/spammer/src/spammer.cpp
+++ Soft_Nodes/spammer/src/spammer.cpp
@@ -20,8 +20,8 @@ int main(int argc, char **argv){ //we need argc and argv for the rosInit functio
 	ros::init(argc, argv, "spam");	//inits the driver
 	ros::NodeHandle n;			//this is what ROS uses to connect to a node
 
-	float out = 0.0f,out2 = 20.0f;
-	unsigned int direc = 0, direc2 = 0;
+	float out = 0.0f,out2 = 20.0f,out3 = 0.0f;
+	unsigned int direc = 0, direc2 = 0, direc3 = 0;
 
 	/*Advertises our various messages*/
 
@@ -29,6 +29,8 @@ int main(int argc, char **argv){ //we need argc and argv for the rosInit functio
 	ros::Publisher spamMsg2 = n.advertise<std_msgs::Float32>("compassHeading", 100);
 	ros::Publisher spamMsg3 = n.advertise<std_msgs::Float32>("pilotDepth", 100);
 	ros::Publisher spamMsg4 = n.advertise<std_msgs::Float32>("svpDepth", 100);
+	ros::Publisher spamMsg5 = n.advertise<std_msgs::Float32>("pilotPitch", 100);
+	ros::Publisher spamMsg6 = n.advertise<std_msgs::Float32>("compassPitch", 100);
 
 	/*Sets up the message structures*/
 
@@ -36,6 +38,8 @@ int main(int argc, char **argv){ //we need argc and argv for the rosInit functio
 	std_msgs::Float32 compassHeading;
 	std_msgs::Float32 pilotDepth;
 	std_msgs::Float32 svpDepth;
+	std_msgs::Float32 pilotPitch;
+	std_msgs::Float32 compassPitch;
 
 	ros::Rate loop_rate(10); //how many times a second (i.e. Hz) the code should run
 
@@ -73,8 +77,24 @@ int main(int argc, char **argv){ //we need argc and argv for the rosInit functio
 
 		svpDepth.data = out2;
 		
+		if(direc3){
+			out3 += 0.2f;
+			if(out3 > 20.0){
+				direc3 = 0;
+			}
+		}
+		else{
+			out3 -= 0.2f;
+			if(out3 < -20.0){
+				direc3 = 1;
+			}
+		}
+		
+		compassPitch.data = out3;
+		
 		pilotHeading.data = 0.0f;
 		pilotDepth.data = 30.0f;
+		pilotPitch.data = 0.0f;
 
 		/*Below here we publish our readings*/
 
@@ -82,6 +102,8 @@ int main(int argc, char **argv){ //we need argc and argv for the rosInit functio
 		spamMsg2.publish(compassHeading);
 		spamMsg3.publish(pilotDepth);		
 		spamMsg4.publish(svpDepth);
+		spamMsg5.publish(pilotPitch);		
+		spamMsg6.publish(compassPitch);
 
 		/*Have a snooze*/
 
