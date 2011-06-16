15fda132c6a9129f987a094383be71ba1edf6e72
everto151 <jamestaylor151@gmail.com>
2011-06-16 10:47:18 +0100

    orange tracker is working in C, just need to fit it inside of ros

--- Drivers/fwdcam/src/C_Testing/orange.c
+++ Drivers/fwdcam/src/C_Testing/orange.c
@@ -75,6 +75,63 @@ void something(void){
 	return;
 }
 
+void somethingNew(void){
+
+	int x, y, count, x_estimate, y_estimate, x_centre, y_centre;
+	int x_draw1, x_draw2, y_draw1, y_draw2;
+	CvScalar s;
+
+	x_estimate = y_estimate = count = 0;
+	
+	for(x=0;x<g_grey->height;x++){
+		for(y=0;y<g_grey->width;y++){
+			s = cvGet2D(g_grey,x,y);
+			if(s.val[0] == 0){
+				count++;
+				x_estimate += x;
+				y_estimate += y;
+			}
+		}
+	}
+
+	if(count == 0){
+		printf("No Object in sight\n");
+	}
+	else{
+		x_centre = y_estimate / count;
+		y_centre = x_estimate / count;
+	}
+
+	x_draw1 = x_centre + 100;
+	x_draw2 = x_centre - 100;
+	y_draw1 = y_centre + 100;
+	y_draw2 = y_centre - 100;
+
+	s.val[0] = 255;
+	
+
+	inline CvPoint pt1 = {x_draw1,y_centre};
+	inline CvPoint pt2 = {x_draw2,y_centre};
+	inline CvPoint pt3 = {x_centre,y_draw1};
+	inline CvPoint pt4 = {x_centre,y_draw2};
+
+	cvLine(g_grey, pt1 , pt2, s, 1, 8,0);
+	cvLine(g_grey, pt3 , pt4, s, 1, 8,0);
+
+	s.val[0] = 0;
+	s.val[1] = 255;
+	s.val[2] = 0;
+
+	cvLine(g_gray, pt1 , pt2, s, 1, 8,0);
+	cvLine(g_gray, pt3 , pt4, s, 1, 8,0);
+
+	printf("X: %u Y: %u\n",x_centre,y_centre);
+
+	return;
+}
+
+	
+
 int main(int argc, char** argv){
 	// Set up variables
 	
@@ -83,16 +140,17 @@ int main(int argc, char** argv){
 	capture = cvCaptureFromCAM(0);
 
 	// create a new window with auto sizing
-	cvNamedWindow(window_name, 1);
+	//cvNamedWindow(window_name, 1);
 
 	// create a window
-	cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE); 
+	//cvNamedWindow("HSV Window", CV_WINDOW_AUTOSIZE); 
 
 	// create a window
-	cvNamedWindow("blobWin", CV_WINDOW_AUTOSIZE);
+	//cvNamedWindow("Grey Window", CV_WINDOW_AUTOSIZE);
 
 	// create a window
-	cvNamedWindow("blobOut", CV_WINDOW_AUTOSIZE);  
+	cvNamedWindow("Target", CV_WINDOW_AUTOSIZE);  
+	cvNamedWindow("Target2", CV_WINDOW_AUTOSIZE);  
 
 	while(quit != 'q'){
 
@@ -100,23 +158,28 @@ int main(int argc, char** argv){
 		g_image = cvQueryFrame(capture);
 
 		// display frame
-		cvShowImage(window_name,g_image);
+		//cvShowImage(window_name,g_image);
 
 		on_trackbar(0);
 
-		cvShowImage("mainWin", g_gray );
+		//cvShowImage("HSV Window", g_gray );
 
-		cvShowImage("blobWin", g_grey );
+		//cvShowImage("Grey Window", g_grey );
 
-		something();
+		somethingNew();
 
-		cvShowImage("blobOut", g_grey );
+		cvShowImage("Target", g_grey );
+		cvShowImage("Target2", g_gray );
+
+		/*something();
+
+		cvShowImage("blobOut", g_grey );*/
 
 		//printf("Thresh is %d\n",g_thresh++);
 	
-		if(g_thresh == 255){
+		/*if(g_thresh == 255){
 			g_thresh = 0;
-		}
+		}*/
 
 		//check for q to quit
 		quit = cvWaitKey(1);
