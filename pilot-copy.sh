0f4b502ce2ed3a99cd0bae02bd56f9ede9906966
everto151 <jamestaylor151@gmail.com>
2011-06-21 11:10:32 +0100

    Couple of changes to the start scripts and copy scipts

--- pilot-copy.sh
+++ pilot-copy.sh
@@ -1,1 +1,1 @@
-sudo rsync -azvv -e ssh ~/ProjectRinzler/ uwesub@192.168.2.10:/home/uwesub/ProjectRinzler/ --exclude-from 'pilot-exclude.txt'
+sudo rsync -azvv -e ssh ~/ProjectRinzler/ uwesub@192.168.2.10:/home/uwesub/ProjectRinzler/ --exclude-from 'pilot-exlude.txt'
