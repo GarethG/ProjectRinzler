105bf378024311e094883597b7390cb87b18b029
James Taylor <jamestaylor151@gmail.com>
2011-07-05 12:55:10 +0200

    Transfer updates

--- camlaunch.sh
+++ camlaunch.sh
@@ -8,6 +8,6 @@ export ROS_MASTER_URI=http://192.168.2.10:11311
 export ROS_IP=192.168.2.20
 export GSCAM_CONFIG="v4l2src device=/dev/video0 ! video/x-raw-rgb ! ffmpegcolorspace"
 
-sleep 5
+sleep 2
 
 roslaunch /home/uwesub/ProjectRinzler/Camera.launch
